bc52328ed1a0b22f5ca819964594a9a24fdf9788
Kevin Zeidler <kzeidler@gmail.com>
2020-06-18 16:31:38 -0700

    use cuda if possible; split inputs into batches to prevent OOM errors (and recover from OOM errors that happen during runtime)

--- ner/ner/run.py
+++ ner/ner/run.py
@@ -3,46 +3,75 @@ import spacy
 from allennlp.predictors import Predictor
 import allennlp
 import allennlp_models
+import textwrap
+import datetime
 from collections import defaultdict
 import re
 import json
 from gemeinsprache.utils import red, green, cyan, magenta, blue, yellow
-
+import torch
 LIMIT_ARTICLES = 100
 
 db = init_db()
 crawldb = db["articles"]
 allennlp_model = None
+
 import allennlp_models.tagging
 
-predictor = Predictor.from_path(
-    "https://storage.googleapis.com/allennlp-public-models/ner-model-2020.02.10.tar.gz"
-)
 
 
-def extract_entities_with_allennlp(s):
+def extract_entities_with_allennlp(*s):
     model_url = "https://storage.googleapis.com/allennlp-public-models/ner-model-2020.02.10.tar.gz"
     global allennlp_model
     if not "allennlp_model" in globals() or not globals()["allennlp_model"]:
-        print(f"Loading AllenNLP NER model...")
-        allennlp_model = Predictor.from_path(model_url)
-        print(f"Load complete.")
-    results = allennlp_model.predict(sentence=s)
-    # print(json.dumps(results))
-    ents = []
-    curr = []
-    for word, tag in zip(results["words"], results["tags"]):
-        if not re.search(r"(LOC)", tag):
-            continue
-        elif word.startswith("'"):
-            curr[-1] += word
+        print(yellow("[ model_init ]"), f" :: Loading AllenNLP NER model...")
+
+        if torch.cuda.is_available():
+            cuda_device = 0
         else:
-            curr.append(word)
-        if tag[0] in "LU":
-            span = " ".join(curr)
-            if len(span) >= 3:
-                ents.append(span)
-            curr = []
+            cuda_device = -1
+        allennlp_model = Predictor.from_path(model_url, cuda_device=cuda_device)
+        print(yellow(f"[ model_init ] "), f" :: CUDA initialized? ", [green("YES"), red("NO")][abs(cuda_device)])
+        print(yellow(f"[ model_init ] "), f" :: Load complete.")
+    print(yellow(f"[ model_predict ]"), f" :: Extracting entities...")
+    start = datetime.datetime.now()
+    ents = []
+    for i, part in enumerate(s):
+        if not part:
+            continue
+        elif len(part) <= 36:
+            part = f"{part} . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . ."
+        curr = []
+        try:
+            print(yellow(f"[ model_predict ]"), " :: Next input:")
+            for line in textwrap.wrap(part):
+                print("                ", cyan(line))
+
+            results = allennlp_model.predict(sentence=part)
+            print(yellow(f"[ model_predict ]"), green(" :: OK"))
+        except Exception as e:
+            print(yellow(f"[ model_predict ]"), red(" :: {e.__class__.__name__}! :: {e}"))
+            continue
+        for word, tag in zip(results["words"], results["tags"]):
+            if not re.search(r"(LOC)", tag):
+                continue
+            elif word.startswith("'"):
+                curr[-1] += word
+            else:
+                curr.append(word)
+            if tag[0] in "LU":
+                span = " ".join(curr)
+                if len(span) >= 3:
+                    ents.append(span)
+                curr = []
+    finish = datetime.datetime.now()
+    elapsed = (finish - start).total_seconds()
+    mins, secs = elapsed // 60, elapsed % 60
+    human_readable = f"{magenta(str(int(mins)).zfill(2))}m {blue(str(int(secs)).zfill(2))}s"
+    # print(json.dumps(results))
+    print(yellow(f"[ model_predict ]"), f" :: Extraction complete.")
+    print(yellow(f"[ model_predict ]"), f" :: Elapsed time : ", human_readable)
+
     print(
         "======================================================================================================"
     )
@@ -55,7 +84,7 @@ def extract_entities_with_allennlp(s):
         cased[ent.lower()].append(ent)
     for k, v in cased.items():
         cased[k] = list(sorted(v, key=lambda x: v.count(x)))
-    freqs = {v[-1]: len(v) for v in cased.values()}
+    freqs = {v[-1]: len(v) for v in sorted(list(cased.values()), key=len, reverse=True)}
     print(blue("Extracted entities:"))
     print(cyan(json.dumps(freqs, indent=4)))
     return freqs
@@ -72,20 +101,20 @@ if __name__ == "__main__":
         for row in crawldb.find(ner=None, prediction="approved", _limit=LIMIT_ARTICLES)
     ]:
         print(f"Updating {row}")
-        content = pad.join(
-            [
-                re.sub(r"(\s*\n+\s*)", pad, str(attr))
-                for attr in [
-                    row["title"],
-                    row["loc"],
-                    row["state"],
-                    row["description"],
-                    row["content"],
-                ]
-                if attr
-            ]
-        )
-        counts = extract_entities_with_allennlp(content)
+        # content = pad.join(
+        #     [
+        #         re.sub(r"(\s*\n+\s*)", pad, str(attr))
+        #         for attr in [
+        #             row["title"],
+        #             row["loc"],
+        #             row["state"],
+        #             row["description"],
+        #             row["content"],
+        #         ]
+        #         if attr
+        #     ]
+        # )
+        counts = extract_entities_with_allennlp(row['title'], row['loc'], row['state'], row['description'], *row['content'].split("\n"))
         updates.append({"url": row["url"], "ner": counts})
 
     crawldb.update_many(updates, ["url"])
