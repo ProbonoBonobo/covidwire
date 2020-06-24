8da31c61a54390628f1e2b636ccd3c8157d05cec
KZ's Droplet <kzeidler@gmail.com>
2020-06-24 04:47:10 +0000

    pushing run.sh

--- spider/spider/run.sh
+++ spider/spider/run.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
-python crawl_sitemaps.py 
-python parse_responses.py 
-python process_queue.py
+if test -f "~/covidwire/spider/spider.lock"; then
+	exit 0
+else
+	touch ~/covidwire/spider/spider.lock
+	exec /root/.pyenv/shims/poetry run python crawl_sitemaps.py > ~/covidwire/spider/crawl_sitemaps.log 
+	exec /root/.pyenv/shims/poetry run python parse_responses.py > ~/covidwire/spider/parse_responses.log
+	exec /root/.pyenv/shims/poetry run python process_queue.py > ~/covidwire/spider/process_queue.log
+	rm ~/covidwire/spider/spider.lock
+fi
