7080dbec1b0cc0342a359c7dc49d35556cead556
Kevin Zeidler <kzeidler@gmail.com>
2020-07-06 19:49:33 -0700

    improvements to sitemap parsing

--- spider/spider/parse_responses.py
+++ spider/spider/parse_responses.py
@@ -1,5 +1,6 @@
 import pickle
 import datetime
+import json
 from spider.common import config, init_db
 from gemeinsprache.utils import cyan, red, blue, green, yellow, magenta
 from dateutil.parser import parse as parse_timestamp
@@ -24,15 +25,14 @@ def ensure_tztime(ts):
         return ts
 
 
-def parse_sitemap(row):
+def parse_sitemap(row, seen):
     if not row or not row["content"]:
-        return []
+        return [], set()
     sitemap_url = row["url"]
 
     soup = BeautifulSoup(row["content"], "xml")
     elements = soup.findAll("url")
     rows = []
-    print(row.keys())
     for elem in elements:
         url_node = elem.find("loc")
         lastmod_node = elem.find("lastmod")
@@ -64,17 +64,19 @@ def parse_sitemap(row):
                 "xmlmeta": xmlmeta,
                 "is_dumpsterfire": row['is_dumpsterfire']
             }
-            rows.append(row)
-            seen.add(url.strip())
+            if row['url'] not in seen:
+                rows.append(row)
+                print(blue(json.dumps(row, indent=4, default=lambda x: str(x) if isinstance(x, (bytes, datetime.datetime)) else x)))
+                seen.add(url.strip())
+            if len(rows) > MAX_ARTICLES_PER_SOURCE:
+                break
 
-    rows = list(sorted(rows, key=lambda row: ensure_tztime(["lastmod"]), reverse=True))[
-        : min(len(rows), MAX_ARTICLES_PER_SOURCE)
-    ]
+    rows = list(sorted(rows, key=lambda row: ensure_tztime(row["lastmod"]), reverse=True))
     print(
         magenta("[ fetch_sitemap ] "),
         f":: Extracted {len(rows)} urls from sitemap: {sitemap_url}",
     )
-    return rows
+    return rows, seen
 
 
 if __name__ == "__main__":
@@ -82,7 +84,9 @@ if __name__ == "__main__":
     responsedb = db["sitemaps"]
     spiderqueue = db["spiderqueue"]
     dumpsterfire = db['dumpsterfire']
-    seen.update([row["url"] for row in spiderqueue])
+    seen.update([row["url"] for row in db.query("select url from spiderqueue")])
+    seen.update([row['url'] for row in db.query("select url from articles")])
+    seen.update([row['url'] for row in db.query("select url from dumpsterfire")])
     # seen.update([row['url'] for row in dumpsterfire])
 
 
@@ -90,9 +94,15 @@ if __name__ == "__main__":
 
     parsed = []
     for row in queue:
-        parsed.extend(parse_sitemap(row))
-    filtered = [row for row in parsed if row['url'] not in seen]
-    for row in filtered:
-        spiderqueue.insert(row, ['url'])
-        print(f"Inserted {row['url']}")
+        rows, _seen = parse_sitemap(row, seen)
+        seen.update(_seen)
+        parsed.extend(rows)
+        if rows:
+            print(f"Inserting {len(rows)} rows...")
+            spiderqueue.upsert_many(rows, ['url'])
+            print(f"Insert complete.")
+    # print(f"Upserting {len(parsed)} rows...")
+    # spiderqueue.insert_many(parsed, ['url'])
+    print(f"Update complete.")
+
 
