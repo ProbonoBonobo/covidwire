e67c7cb26047d183582ba643c327890667eebdab
Kevin Zeidler <kzeidler@gmail.com>
2020-08-10 19:45:00 -0700

    add regional filters and deduplicate identical titles

--- api/api/run.py
+++ api/api/run.py
@@ -1,6 +1,7 @@
 import flask
 from flask import request
 from api.creds import db_config
+from scipy.spatial.distance import euclidean, cosine
 import datetime
 import dataset
 import json
@@ -16,7 +17,8 @@ articles = db['articles']
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
-
+import numpy as np
+from collections import defaultdict
 
 
 @app.route('/', methods=['GET'])
@@ -29,11 +31,11 @@ def home():
     formatted_results = '</div><div>'.join([json.dumps(result, indent=4, default=lambda x: x if isinstance(x, (str, int, float, list)) else str(x)).replace(" ", "&nbsp;").replace("\n", "<br>") for result in results])
     return f"""<h1>Query Parameters:</h1><p>{formatted_args}</p>
                <h2>Results:</h2><div>{formatted_results}</div>"""
-
+import random
 @app.route('/scored', methods=['GET'])
 def get_scored_results():
     print(f"Cache keys: {list(cache.keys())}")
-    kwargs = {"_limit": 50, "p": 0, "fips": "06097"}
+    kwargs = {"_limit": 50, "p": 0, "fips": "06097", "audience": ""}
     _kwargs = {k: urldecode(v) for k,v in request.args.items()} or {}
     kwargs.update(_kwargs)
 
@@ -46,26 +48,31 @@ def get_scored_results():
     if fips in cache and cache[fips] and cache[fips][0] > time.time():
         results = cache[fips][1]
     else:
-        query = f"""SELECT distinct on (title, published_at,   cast(scored ->> '{fips}' as float) / (extract(days from (now() - published_at)) * 2 + 1))
-        id, published_at, name, title, description, content, url, docvec, ner, cast(scored ->> '{fips}' as float)  / (extract(days from (now() - published_at)) * 2 + 1) as score
+        query = f"""SELECT distinct on (title)
+        id, published_at, name, title, description, content, url, docvec, ner, image_url, audience, cast(scored ->> '{fips}' as float)  / (extract(days from (now() - published_at)) * 2 + 1) as score
     FROM
         articles
-    WHERE scored ->> '{fips}' != '0'
-    ORDER BY
-        cast(scored ->> '{fips}' as float) / (extract(days from (now() - published_at)) * 2 + 1) desc;"""
-        print(query)
-        results = list(db.query(query))
-        for r in results:
+    WHERE scored ->> '{fips}' != '0';"""
+        results = []
+        for r in sorted(db.query(query), key=lambda x: x['score'], reverse=True):
             try:
                 r['docvec'] = list(map(float,r['docvec'][1:-1].split(",")))
+
+
             except:
-                r['docvec'] = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+                r['docvec'] = [x*0.00000001 for x in random.sample(range(-70000000,70000000), 8)]
+            results.append(r)
+
         cache[fips] = [time.time() + 3200, results]
     #print(results[start:stop])
     #ormatted_args = '</p><p>'.join([f"{k} = {v}\n" for k, v in request.args.items()])
     #formatted_results = '</div><div>'.join([json.dumps(result, indent=4, default=str).replace(" ", "&nbsp;").replace("\n", "<br>").replace("\\n", "<br>") for result in results[start:stop]])
+    if kwargs['audience']:
+        audience = kwargs['audience']
+        results = [result for result in results if result['audience'] == audience]
+    results = results[start:stop]
     response = app.response_class(
-        response = json.dumps(results[start:stop], indent=4, default=lambda x: x if not isinstance(x, datetime.datetime) else x.isoformat()),
+        response = json.dumps(results, indent=4, default=lambda x: x if not isinstance(x, datetime.datetime) else x.isoformat()),
         status=200,
         mimetype = 'application/json')
     return response
