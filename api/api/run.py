c91dcb7204f228258ec6fa3f416418b0fd720c85
Kevin Zeidler <kzeidler@gmail.com>
2020-08-10 18:28:54 -0700

    implement pagination and caching; return JSON responses

--- api/api/run.py
+++ api/api/run.py
@@ -1,6 +1,7 @@
 import flask
 from flask import request
 from api.creds import db_config
+import datetime
 import dataset
 import json
 import time
@@ -33,15 +34,17 @@ def home():
 def get_scored_results():
     print(f"Cache keys: {list(cache.keys())}")
     kwargs = {"_limit": 50, "p": 0, "fips": "06097"}
-    kwargs.update({k: urldecode(v) for k,v in request.args.items()} or {})
+    _kwargs = {k: urldecode(v) for k,v in request.args.items()} or {}
+    kwargs.update(_kwargs)
 
     fips = kwargs['fips']
+    print(f"fips is : {fips}")
     start = int(kwargs['_limit']) * int(kwargs['p'])
     stop = start + int(kwargs['_limit'])
     print(f"Start is: {start}, Stop is: {stop}")
     results = None
     if fips in cache and cache[fips] and cache[fips][0] > time.time():
-        results = cache[fips][1][start:stop]
+        results = cache[fips][1]
     else:
         query = f"""SELECT distinct on (title, published_at,   cast(scored ->> '{fips}' as float) / (extract(days from (now() - published_at)) * 2 + 1))
         id, published_at, name, title, description, content, url, docvec, ner, cast(scored ->> '{fips}' as float)  / (extract(days from (now() - published_at)) * 2 + 1) as score
@@ -50,15 +53,24 @@ def get_scored_results():
     WHERE scored ->> '{fips}' != '0'
     ORDER BY
         cast(scored ->> '{fips}' as float) / (extract(days from (now() - published_at)) * 2 + 1) desc;"""
-
         print(query)
         results = list(db.query(query))
+        for r in results:
+            try:
+                r['docvec'] = list(map(float,r['docvec'][1:-1].split(",")))
+            except:
+                r['docvec'] = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
         cache[fips] = [time.time() + 3200, results]
-    print(results[start:stop])
-    formatted_args = '</p><p>'.join([f"{k} = {v}\n" for k, v in request.args.items()])
-    formatted_results = '</div><div>'.join([json.dumps(result, indent=4, default=str).replace(" ", "&nbsp;").replace("\n", "<br>").replace("\\n", "<br>") for result in results[start:stop]])
-    return f"""<h1>Query Parameters:</h1><p>{formatted_args}</p>
-                   <h2>Results:</h2><div>{formatted_results}</div>"""
+    #print(results[start:stop])
+    #ormatted_args = '</p><p>'.join([f"{k} = {v}\n" for k, v in request.args.items()])
+    #formatted_results = '</div><div>'.join([json.dumps(result, indent=4, default=str).replace(" ", "&nbsp;").replace("\n", "<br>").replace("\\n", "<br>") for result in results[start:stop]])
+    response = app.response_class(
+        response = json.dumps(results[start:stop], indent=4, default=lambda x: x if not isinstance(x, datetime.datetime) else x.isoformat()),
+        status=200,
+        mimetype = 'application/json')
+    return response
+    # return f"""<h1>Query Parameters:</h1><p>{formatted_args}</p>
+    #                <h2>Results:</h2><div>{formatted_results}</div>"""
 
-app.run(port=8888)
+app.run(host='0.0.0.0', port=8888)
 
