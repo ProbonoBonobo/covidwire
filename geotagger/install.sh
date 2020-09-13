c59a08a0002722ea1c7798ac5241c8dd1d36c00d
Kevin Zeidler <kzeidler@gmail.com>
2020-09-12 21:28:25 -0700

    installer update

--- geotagger/install.sh
+++ geotagger/install.sh
@@ -3,7 +3,7 @@ echo 'export CFLAGS="-O2"' >> ./.env
 source ./.env
 mkdir -p $MODEL_FILEPATH
 cd $MODEL_FILEPATH || return
-apt install -y pzip-full wget make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm \
+apt install -y p7zip-full wget make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm \
 libncurses5-dev libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev python-openssl git
 mkdir model
 wget "http://www.kevinzeidler.com/$MODEL_FILENAME"
@@ -15,8 +15,8 @@ if [ -d "/path/to/dir" ]
     echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
     echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n eval "$(pyenv init -)"\nfi' >> ~/.bashrc
     exec $SHELL || return
-    pyenv install $PYTHON_VERSION
 fi
+pyenv install $PYTHON_VERSION
 pyenv local $PYTHON_VERSION
 pip install --upgrade pip
 pip install poetry
