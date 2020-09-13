f69165855e0060c87b9df0e27ef19acb46697465
Kevin Zeidler <kzeidler@gmail.com>
2020-09-12 20:52:58 -0700

    install python version 3.6.8 to match required python version for pickled ktrain model

---
+++ geotagger/install.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+apt install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm \
+libncurses5-dev libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev python-openssl git
+git clone https://github.com/pyenv/pyenv.git
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
+echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n eval "$(pyenv init -)"\nfi' >> ~/.bashrc
+exec "$SHELL" || return
+pyenv install $PYTHON_VERSION
