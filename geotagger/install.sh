bfc8c9bb383683478a0924d6cfeee506cd84fd3b
Kevin Zeidler <kzeidler@gmail.com>
2020-09-12 21:18:29 -0700

    installer update

--- geotagger/install.sh
+++ geotagger/install.sh
@@ -1,9 +1,21 @@
 #!/usr/bin/env bash
-apt install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm \
+echo 'export CFLAGS="-O2"' >> ./.env
+source ./.env
+mkdir -p $MODEL_FILEPATH
+cd $MODEL_FILEPATH || return
+apt install -y pzip-full wget make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm \
 libncurses5-dev libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev python-openssl git
-git clone https://github.com/pyenv/pyenv.git
+mkdir model
+wget "http://www.kevinzeidler.com/$MODEL_FILENAME"
+7za e "$MODEL_FILENAME"
+git clone https://github.com/pyenv/pyenv.git ~/.pyenv
 echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
 echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
 echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n eval "$(pyenv init -)"\nfi' >> ~/.bashrc
-exec "$SHELL" || return
+exec $SHELL || return
 pyenv install $PYTHON_VERSION
+pyenv local $PYTHON_VERSION
+pip install --upgrade pip
+pip install poetry
+poetry run pip install --upgrade pip
+poetry update
